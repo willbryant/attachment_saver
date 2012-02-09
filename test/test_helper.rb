87d0c29f452727cebb9f35afd4002e7ce78b8d36
Will Bryant <will.bryant@gmail.com>
2012-02-09 21:42:02 +1300

    update the test_helper.rb code to cope with the sqlite adapter attempting to put out logger messages, and support RAILS_VERSION env var like my other plugins

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,17 @@
-require 'rubygems'
-
-PROJECT_ROOT=File.expand_path("../../..")
-if File.directory?("#{PROJECT_ROOT}/vendor/rails")
-  require "#{PROJECT_ROOT}/vendor/rails/railties/lib/initializer"
+if File.exist?("../../../config/boot.rb")
+  require "../../../config/boot.rb"
+else
+  require 'rubygems'
 end
+
+gem 'activesupport', ENV['RAILS_VERSION']
+gem 'activerecord',  ENV['RAILS_VERSION']
+
+require 'test/unit'
+require 'active_support'
+require 'active_support/test_case'
 require 'active_record'
+require 'active_record/fixtures'
 
 begin
   require 'ruby-debug'
@@ -22,14 +29,21 @@ class Rails
   def self.env;  RAILS_ENV;  end
 end
 
-database_config = YAML::load(IO.read(File.join(File.dirname(__FILE__), '/database.yml')))
-ActiveRecord::Base.establish_connection(database_config[ENV['RAILS_ENV']])
+class DummyLogger
+  def method_missing(*args)
+  end
+end
+
+ActiveRecord::Base.logger = DummyLogger.new
+
+ActiveRecord::Base.configurations = YAML::load(IO.read(File.join(File.dirname(__FILE__), "database.yml")))
+ActiveRecord::Base.establish_connection ActiveRecord::Base.configurations[ENV['RAILS_ENV']]
 load(File.join(File.dirname(__FILE__), "/schema.rb"))
 
-require File.expand_path(File.join(File.dirname(__FILE__), '../init')) # load attachment_saver
+require File.expand_path(File.join(File.dirname(__FILE__), '../init')) # load the plugin
 
-at_exit do # at_exits are run in reverse of declaration order, and Test::Unit runs from an at_exit, so we must declare ours before that jrequire below
-  FileUtils.rm_rf(File.join(Rails.root, 'public', 'test'))
+at_exit do # at_exits are run in reverse of declaration order, and Test::Unit runs from an at_exit, so we must declare ours before that require below
+  FileUtils.rm_rf(File.join(File.dirname(__FILE__), 'tmp'))
 end
 
 require 'test/unit'
